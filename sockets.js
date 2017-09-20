9f3582e83856cfcd2e66e61180b8682b1064d737
ira <ira@1thrasher.com>
2017-09-20 14:22:44 -0500

    removed io.origins; updated stunservers

--- sockets.js
+++ sockets.js
@@ -4,7 +4,6 @@ var socketIO = require('socket.io'),
 
 module.exports = function (server, config) {
     var io = socketIO.listen(server);
-    io.set('origins', '*:*');
 
     io.sockets.on('connection', function (client) {
         client.resources = {
