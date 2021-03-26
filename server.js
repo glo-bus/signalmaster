a6e9bf9f1eb08ad749af90e2b3b752b05a394fe9
ira <ira@1thrasher.com>
2021-03-26 12:26:10 -0500

    removed secure server code to match sim-signalmaster

--- server.js
+++ server.js
@@ -11,15 +11,15 @@ var yetify = require('yetify'),
     server = null;
 
 // Create an http(s) server instance to that socket.io can listen to
-if (config.server.secure) {
-    server = require('https').Server({
-        key: fs.readFileSync(config.server.key),
-        cert: fs.readFileSync(config.server.cert),
-        passphrase: config.server.password
-    }, server_handler);
-} else {
-    server = require('http').Server(server_handler);
-}
+// if (config.server.secure) {
+//     server = require('https').Server({
+//         key: fs.readFileSync(config.server.key),
+//         cert: fs.readFileSync(config.server.cert),
+//         passphrase: config.server.password
+//     }, server_handler);
+// } else {
+server = require('http').Server(server_handler);
+// }
 server.listen(port);
 
 sockets(server, config);
